d0bf5904f185d27d0e676f51e5ecb9cca7e1d7c7
sorin.munteanu <sorinmunteanu25@gmail.com>
2021-06-16 21:07:39 +0300

    Added detail, newticket, editticket and index html pages and linked them through links and buttons. Known issue: link between detail and editticket not working - issue in dynamic pathing.

--- Service_Web_Platform/ticket/urls.py
+++ Service_Web_Platform/ticket/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'ticket'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:ticket_number>/', views.detail, name='detail'),
+    path('<int:ticket_number>/editticket/', views.editticket, name='editticket'),
+    path('newticket/', views.newticket, name='newticket'),
 ]
--- Service_Web_Platform/ticket/views.py
+++ Service_Web_Platform/ticket/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+from .models import Ticket
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the ticket homepage.")
+    latest_ticket_list = Ticket.objects.order_by('-ticket_number')
+    context = {
+        'latest_ticket_list': latest_ticket_list,
+    }
+    return render(request, 'ticket/index.html', context)
+
+def detail(request, ticket_number):
+    ticket = get_object_or_404(Ticket, pk=ticket_number)
+    return render(request, 'ticket/detail.html', {'ticket': ticket})
+
+def newticket(request):
+    return render(request, 'ticket/newticket.html')
+
+def editticket(request):
+    return render(request, '<int:ticket_number>/editticket.html')
